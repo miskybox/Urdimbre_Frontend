077c89116888d73d9050879cb0f7ad82e08f68f2
RieraDiPe <rieradipe@gmail.com>
2025-06-06 11:22:32 +0200

    feat: create ProffesionalsCard component width responsive base style

---
+++ src/components/professionalsCard/ProfessionalsCard.jsx
@@ -0,0 +1,32 @@
+import Rect from "react";
+import style from "./ProfessionalsCard.module.css";
+
+const ProfessionalsCard = ({
+  image,
+  nProfesional,
+  description,
+  location,
+  price,
+}) => {
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={nProfesional} className={styles.image} />
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <h2 className={styles.name}>{nProfesional}</h2>
+          <div className={styles.price}>
+            <span>{price}€</span>
+            <small>/h</small>
+          </div>
+        </div>
+
+        <div className={styles.subinfo}>
+          <span className={styles.location}>📍 {location}</span>
+        </div>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfessionalsCard;
